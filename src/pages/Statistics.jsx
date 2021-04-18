23f15edbe4f81da43d118e77e95873c32ca0b172
Woodpeaker <49099503+Woodpeaker@users.noreply.github.com>
2021-04-18 01:32:13 -0400

    connected statistics  to database

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,26 +1,54 @@
-import React, { useEffect } from 'react'
+import React, { useEffect ,useState} from 'react'
 import { SafeAreaView, Text, View, StyleSheet, Dimensions, ScrollView ,StatusBar} from 'react-native';
 import {Card, Title, Headline} from 'react-native-paper';
+import { firebase,db } from '../../database/firebase';
 import { LineChart, BarChart, PieChart, ProgressChart, ContributionGraph, StackedBarChart} from 'react-native-chart-kit';
 import TabBar from '../components/TabBar.jsx';
 
-const data = [
-  { name: 'Entertainment', cost: 21, color: '#FFC542', legendFontColor: '#7F7F7F', legendFontSize: 11 },
-  { name: 'Music', cost: 18, color: '#FF575F', legendFontColor: '#7F7F7F', legendFontSize: 11 },
-  { name: 'Gaming', cost: 12, color: '#3DD598', legendFontColor: '#7F7F7F', legendFontSize: 11 },
-  { name: 'Other', cost: 6, color: '#6691FF', legendFontColor: '#7F7F7F', legendFontSize: 11 },
-  ]
 
 
 export default function Statistics({navigation}) {
-
+  const [Services, setServices] = useState();
+  const [stats, setStats] = useState();
+  let data = [
+    { name: 'Entertainment', cost: 0, color: '#FFC542', legendFontColor: '#7F7F7F', legendFontSize: 11 },
+    { name: 'Music', cost: 0, color: '#FF575F', legendFontColor: '#7F7F7F', legendFontSize: 11 },
+    { name: 'Gaming', cost: 0, color: '#3DD598', legendFontColor: '#7F7F7F', legendFontSize: 11 },
+    { name: 'Other', cost: 0, color: '#6691FF', legendFontColor: '#7F7F7F', legendFontSize: 11 },
+  ]
   useEffect(()=>{
     if (Platform.OS == 'android') {
       StatusBar.setBarStyle('light-content', true)
       StatusBar.setBackgroundColor("#2A3C44")
     }
-
+    _fetchServics();
   }, [])
+
+  const _fetchServics = async () =>{
+
+    var docRef = db.collection("users").doc("test1");
+
+    docRef.get().then((doc) => {
+      if (doc.exists) {
+        let Service = doc.data().services
+        setServices(Service)
+        console.log(data)
+        for (var i = 0;i<Service.length;i++)
+        {
+          if(Service[i].Category=="Entertainment")
+            data[0].cost+=Service[i].packages
+        }
+      console.log(data[0].cost)
+        setStats(data)
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+      }
+    }).catch((error) => {
+      console.log("Error getting document:", error);
+    });
+  }
+
     return (
 
         <View>
@@ -28,24 +56,26 @@ export default function Statistics({navigation}) {
                  <Card style={styles.monthlyCard}>
                      <Card.Content>
                         <Text style={styles.header}>Statistics</Text>
-                        <PieChart
-                          data={data}
-                          width={300}
-                          height={175}
-                          chartConfig={{
-                                backgroundColor: '#e26a00',
-                                backgroundGradientFrom: '#fb8c00',
-                                backgroundGradientTo: '#ffa726',
-                                decimalPlaces: 2, // optional, defaults to 2dp
-                                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                                style: {
-                                  borderRadius: 16
-                                }
-                              }}
-                          accessor="cost"
-                          backgroundColor="transparent"
+                       {stats &&
+                         <PieChart
+                             data={stats}
+                             width={300}
+                             height={175}
+                             chartConfig={{
+                               backgroundColor: '#e26a00',
+                               backgroundGradientFrom: '#fb8c00',
+                               backgroundGradientTo: '#ffa726',
+                               decimalPlaces: 2, // optional, defaults to 2dp
+                               color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                               style: {
+                                 borderRadius: 16
+                               }
+                             }}
+                             accessor="cost"
+                             backgroundColor="transparent"
 
-                        />
+                         />
+                       }
                      </Card.Content>
                  </Card>
             </View>
